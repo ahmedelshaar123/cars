518f8ed09db81597fc2cebf7e479bdbc4e383a79
ahmedelshaar123 <elshaar85.com>
2020-11-24 18:06:55 +0200

    elshaar

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -22,6 +22,32 @@ return [
   'model' => 'Model',
   'choose_brand' => 'Choose Brand',
   //services
+  'services' => 'Services',
+  'desc' => 'Description',
+  'features' => 'Features',
+  'images' => 'Images',
+  'desc_ar' => 'Arabic Description',
+  'desc_en' => 'English Description',
+  'features_ar' => 'Arabic Features',
+  'features_en' => 'English Features',
+  //sliders
+  'sliders' => 'Sliders',
+  'image' => 'Image',
+  //years
+  'years' => 'Years',
+  'year' => 'Year',
+  //testimonials
+  'testimonials' => 'Testimonials',
+  'job' => 'Job',
+  'job_ar' => 'Arabic Job',
+  'job_en' => 'English Job',
+  //settings
+  'settings' => 'Settings',
+  //static pages
+  'static_pages' => 'Static Pages',
+
+
+
 
 
 
