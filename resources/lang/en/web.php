831026477ec51f235c6c26143894546b896238eb
ahmedelshaar123 <elshaar85.com>
2020-11-26 16:44:35 +0200

    elshaar

---
+++ resources/lang/en/web.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    //about
+    'over_20_years_experience' => '',
+    'our_message' => '',
+    'our_vision' => '',
+    //testimonials
+    //contact
+    'contact_us' => '',
+    'contact_info' => '',
+    'location' => '',
+    'opening_hours' => '',
+    'message' => '',
+    //galleries
+    'gallery' => '',
+    'read_more' => '',
+    //services
+    'services' => '',
+    //index
+    'full_service_of' => '',
+    'car_repair_and' => '',
+    'maintenance' => '',
+    'prev' => '',
+    'next' => '',
+    'about_us' => '',
+    'over_20_years_of_experiece' => '',
+    'we_repair_and_maintain_all_cars' => '',
+    'brake_repair_and_service' => '',
+    'maintenance_and_repairs' => '',
+    'oil_change_and_filter' => '',
+    'testimonials' => '',
+    'the_car_repair_statistics' => '',
+    'years_of_experience' => '',
+    'repaired_cars' => '',
+    'workers' => '',
+    'car_repair' => '',
+    'details' => '',
+    'get_car_repair_at_lowest_price' => '',
+    'name' => '',
+    'email' => '',
+    'phone' => '',
+    'brand' => '',
+    'model' => '',
+    'year' => '',
+    'needed_repair' => '',
+    'send' => '',
+    'working_times' => '',
+    //header
+    'home' => '',
+    'galleries' => '',
+    //footer
+    'all_rights_reserved' => '',
+];
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -17,10 +17,10 @@
                         </div>
                     </div>
                     <div class="carousel-caption animate__animated animate__backInLeft">
-                        <h4>Full Service Of</h4>
-                        <h2 class=" font-weight-bold display-4  fon_h2 main-color" style="margin-top: -10px;"> CarRepair& </h2>
-                        <h2 class="font-weight-bold pb-2  display-4 fon_h2 main-color top_P" style="margin-top: -10px;"> Maintenance</h2>
-                        <h4 class="h5 ">Over 20 Years Of Quality</h4>
+                        <h4>@lang('web.full_service_of')</h4>
+                        <h2 class=" font-weight-bold display-4  fon_h2 main-color" style="margin-top: -10px;"> @lang('web.car_repair_and') </h2>
+                        <h2 class="font-weight-bold pb-2  display-4 fon_h2 main-color top_P" style="margin-top: -10px;"> @lang('web.maintenance')</h2>
+                        <h4 class="h5 ">@lang('web.over_20_years_of_experiece')</h4>
                     </div>
                 </div>
             @endforeach
@@ -29,14 +29,14 @@
             <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
             <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
 
-            <span class="sr-only">Previous</span>
+            <span class="sr-only">@lang('web.prev')</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleCaptions " role="button" data-slide="next">
             <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
 
             <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
 
-            <span class="sr-only">Next</span>
+            <span class="sr-only">@lang('web.next')</span>
         </a>
 
 
@@ -44,14 +44,14 @@
 </div>
 <div class="car_1 py-5 mt-5">
     <div class="container">
-        <h3 class="font-weight-bold text-center">About Us</h3>
-        <p class=" mb-5 d-block font-weight-bold  position-relative under-line text-center main-color">We repair & maintenance for all car</p>
+        <h3 class="font-weight-bold text-center">@lang('web.about_us')</h3>
+        <p class=" mb-5 d-block font-weight-bold  position-relative under-line text-center main-color">@lang('web.we_repair_and_maintain_all_cars')</p>
         <div class="row">
             <div class="col-lg-6">
                 <img src="{{asset('web/dist/img/header (3).jpg')}}" alt="" class=" mt-4 hvr-pop " width="100%" height="400px" />
             </div>
             <div class="col-lg-6">
-                <h3 class="font-weight-bold mt-5 hvr-float-shadow"> Over 20 Years experience</h3>
+                <h3 class="font-weight-bold mt-5 hvr-float-shadow">@lang('web.over_20_years_of_experiece')</h3>
                 {!! $about->$valueVC !!}
 {{--                <div class=" d-block mt-5">--}}
 {{--                    <a href="#" class="btn fourth  btn-lg pointer py-2  ">More Details</a>--}}
@@ -63,7 +63,7 @@
 <div class="car_2 py-5 mt-5">
     <div class="container">
         <h3 class="font-weight-bold text-center">What We Do</h3>
-        <p class=" mb-5 d-block font-weight-bold  position-relative under-line text-center main-color">We offer full service Car repair & maintenance </p>
+        <p class=" mb-5 d-block font-weight-bold  position-relative under-line text-center main-color">@lang('web.we_repair_and_maintain_all_cars')</p>
         <div class="row">
             <div class="col-lg-4 mt-5-lg mt-4">
                 <img src="{{asset('web/dist/img/pic (10).jpg')}}" alt="" width="100%" height="260px" class="hvr-rotate" />
@@ -71,7 +71,7 @@
             <div class="col-lg-4 mt-5-lg mt-4">
                 <div class="card bg-light border-0 px-5 py-4 hvr-rotate">
                     <div class="text-center pt-4">
-                        <h4 class="mb-4 font-weight-bold">Brake Repair and Service</h4>
+                        <h4 class="mb-4 font-weight-bold">@lang('web.brake_repair_and_service')</h4>
                         <p>Lorem ipsum dolor sit amet aliquid animi officiis aspernatur voluptas porro optio corporis adipisci? Mollitia, explicabo.</p>
                     </div>
                 </div>
@@ -83,7 +83,7 @@
             <div class="col-lg-4 mt-4">
                 <div class="card bg-dark border-0 px-5 py-4 hvr-rotate text-white">
                     <div class="text-center pt-4">
-                        <h4 class="mb-4 font-weight-bold">Maintenance and Repairs</h4>
+                        <h4 class="mb-4 font-weight-bold">@lang('web.maintenance_and_repairs')</h4>
                         <p>Lorem ipsum dolor sit amet aliquid animi officiis aspernatur voluptas porro optio corporis adipisci? Mollitia, explicabo.</p>
                     </div>
                 </div>
@@ -94,7 +94,7 @@
             <div class="col-lg-4 mt-4">
                 <div class="card border-0 px-5 py-4 hvr-rotate oil">
                     <div class="text-center pt-4">
-                        <h4 class="mb-4 font-weight-bold">Oil Change and filter</h4>
+                        <h4 class="mb-4 font-weight-bold">@lang('web.oil_change_and_filter')</h4>
                         <p style="margin-bottom: 2.5rem;">Lorem ipsum dolor sit amet aliquid animi officiis aspernatur voluptas porro optio corporis adipisci? Mollitia, explicabo.</p>
                     </div>
                 </div>
@@ -105,8 +105,8 @@
 </div>
 <div class="car-3 bg-light py-5  ">
     <div class="container">
-        <h3 class="font-weight-bold text-center">Testimonial</h3>
-        <p class=" d-block font-weight-bold  position-relative under-line text-center main-color" style="margin-bottom: 5rem;">Clients opinion car repair & maintenance</p>
+        <h3 class="font-weight-bold text-center">@lang('web.testimonials')</h3>
+        <p class=" d-block font-weight-bold  position-relative under-line text-center main-color" style="margin-bottom: 5rem;">@lang('web.testimonials')</p>
         <div class=" slide text-center ">
             <!-- Slider -->
             <div id="carousel" class="carousel slide" data-ride="carousel">
@@ -135,20 +135,20 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mt-4 order-lg-1 order-12">
-                <h4 class="font-weight-bold mb-3 hvr-float-shadow">The Car Repair Statistics</h4>
+                <h4 class="font-weight-bold mb-3 hvr-float-shadow">@lang('web.the_car_repair_statistics')</h4>
                 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium eaque ipsam ducimus earum labore quo reprehenderit molestiae alias odit, fugit soluta quisquam rem natus ut porro dolorum quis esse reiciendis!</p>
                 <div class="row">
                     <div class="col-lg-4 mt-5 hvr-bob">
                         <h4 class="d-block main-color text-center font-weight-bold">{{$yearsOfExperience->$valueVC}}</h4>
-                        <h5 class="text-center">Years of EXperience</h5>
+                        <h5 class="text-center">@lang('web.years_of_experience')</h5>
                     </div>
                     <div class="col-lg-4 mt-5 hvr-bob">
                         <h4 class="d-block main-color text-center font-weight-bold">{{$repairedCars->$valueVC}}</h4>
-                        <h5 class="text-center">Cars Repaired</h5>
+                        <h5 class="text-center">@lang('web.repaired_cars')</h5>
                     </div>
                     <div class="col-lg-4 mt-5 hvr-bob">
                         <h4 class="d-block main-color text-center font-weight-bold">{{$workers->$valueVC}}</h4>
-                        <h5 class="text-center">Workers & Technicians</h5>
+                        <h5 class="text-center">@lang('web.workers')</h5>
                     </div>
 
                 </div>
@@ -169,23 +169,23 @@
 <div class="car-5 py-5 my-5">
     <div class="container">
         @include('layouts.partials.validation-errors')
-        <h3 class="font-weight-bold text-center text-white h1 ">Car Repair <span class="main-color">Detail</span></h3>
-        <p class=" mb-5 d-block  text-center text-white">Get the car repair at the lowest price</p>
+        <h3 class="font-weight-bold text-center text-white h1 ">@lang('web.car_repair')<span class="main-color">@lang('web.details')</span></h3>
+        <p class=" mb-5 d-block  text-center text-white">@lang('web.get_car_repair_at_lowest_price')</p>
         <form method="post" action="{{route('repaired-car')}}">
         @csrf
             <div class="row">
                 <div class="col-lg-3 ">
-                    <input type="text" placeholder="الاسم" class="form-control mb-5 font-weight-bold text-black-50" name="name">
+                    <input type="text" placeholder="@lang('web.name')" class="form-control mb-5 font-weight-bold text-black-50" name="name">
                 </div>
                 <div class="col-lg-3 ">
-                    <input type="email" placeholder="البريد الالكتروني" class="form-control mb-5 font-weight-bold text-black-50" name="email">
+                    <input type="email" placeholder="@lang('web.email')" class="form-control mb-5 font-weight-bold text-black-50" name="email">
                 </div>
                 <div class="col-lg-3 ">
-                    <input type="number" placeholder="الجوال" class="form-control mb-5 font-weight-bold text-black-50" name="phone">
+                    <input type="number" placeholder="@lang('web.phone')" class="form-control mb-5 font-weight-bold text-black-50" name="phone">
                 </div>
                 <div class="col-lg-3 ">
                     <select class="form-control mb-5 font-weight-bold text-black-50" id="brand_id" name="brand_id">
-                        <option value="" selected disabled> Car Brand</option>
+                        <option value="" selected disabled>@lang('web.brand')</option>
                         @foreach($brands as $brand)
                             <option value="{{$brand->id}}">{{$brand->$nameVC}}</option>
                         @endforeach
@@ -193,12 +193,12 @@
                 </div>
                 <div class="col-lg-2 ">
                     <select class="form-control mb-5 font-weight-bold text-black-50" id="modell_id" name="modell_id[]" >
-                        <option selected disabled> Model</option>
+                        <option selected disabled>@lang('web.model')</option>
                     </select>
                 </div>
                 <div class="col-lg-2 ">
                     <select class="form-control mb-5 font-weight-bold text-black-50" id="exampleFormControlSelect1" name="year">
-                        <option value="" selected disabled> Year</option>
+                        <option value="" selected disabled>@lang('web.year')</option>
                         @foreach($years as $year)
                             <option value="{{$year->year}}">{{$year->year}}</option>
                         @endforeach
@@ -207,7 +207,7 @@
                 <div class="col-lg-3">
                     <select class="form-control mb-5 font-weight-bold text-black-50" id="exampleFormControlSelect1" name="service_id">
 
-                        <option value="" selected disabled> Repair Needed</option>
+                        <option value="" selected disabled>@lang('web.needed_repair')</option>
                         @foreach($services as $service)
                             <option value="{{$service->id}}">{{$service->$nameVC}}</option>
                         @endforeach
@@ -215,7 +215,7 @@
                 </div>
                 <div class="col-lg-2">
                     <a href="#">
-                        <button class="btn btn-info    ml-lg-3 mt-2  btn_color">Send </button>
+                        <button class="btn btn-info    ml-lg-3 mt-2  btn_color">@lang('web.send')</button>
                     </a>
                 </div>
             </div>
@@ -252,7 +252,7 @@
                                                             <h5 class="text-center text-dark font-weight-bold">{{$service->$nameVC}}</h5>
                                                             <p class="text-dark ">{!! $service->$descVC !!}</p>
                                                             <div class=" d-block mt-1 pb-4">
-                                                                <a href="{{url('services')}}" class="btn fourth  btn-lg  pointer   px-3">Read More</a>
+                                                                <a href="{{url('services')}}" class="btn fourth  btn-lg  pointer   px-3">@lang('web.read_more')</a>
                                                             </div>
                                                         </div>
 
@@ -283,7 +283,7 @@
                                                             <h5 class="text-center text-dark font-weight-bold">{{$service->$nameVC}}</h5>
                                                             <p class="text-dark ">{!! $service->$descVC !!}</p>
                                                             <div class=" d-block mt-1 pb-4">
-                                                                <a href="{{url('services')}}" class="btn fourth  btn-lg  pointer   px-3">Read More</a>
+                                                                <a href="{{url('services')}}" class="btn fourth  btn-lg  pointer   px-3">@lang('web.read_more')</a>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -308,7 +308,7 @@
         <div class="cover-map card rounded-0  p-4">
             <div class="row">
                 <div class="col">
-                    <h5 class="d-block text-white mb-4 mt-5 font-weight-bold hvr-grow-rotate">Contact Info</h5>
+                    <h5 class="d-block text-white mb-4 mt-5 font-weight-bold hvr-grow-rotate">@lang('web.contact_info')</h5>
                     <h6 class=" d-block p-arg mr-3 text-white hvr-bob">
                         <i class="fa fa-map-marker mt-2 pr-2 text-white"></i> Location: {{$addressVC->$valueVC}} <span class="d-block" style="padding-left: 1.6rem;"></span>
                     </h6>
@@ -318,7 +318,7 @@
                     <h6 class=" d-block p-arg mr-3 text-white hvr-bob">
                         <i class="fa fa-envelope-o mt-2 pr-2 text-white"></i> {{$emailVC->$valueVC}}
                     </h6>
-                    <h5 class="d-block text-white my-4 text-white font-weight-bold hvr-grow-rotate">Opening Hours</h5>
+                    <h5 class="d-block text-white my-4 text-white font-weight-bold hvr-grow-rotate">@lang('web.working_times')</h5>
                     <h6 class=" d-block p-arg mr-3 text-white hvr-bob">
                         <i class="fa fa-clock mt-2 pr-2 text-white"></i>{{$workTimesVC->$valueVC}}
                     </h6>
